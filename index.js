4cc7e65321240109b1ee1e4e8bea017deb4adc91
mohamed ashif <ashif@gwayit.com>
2022-01-29 16:06:47 +0530

    some changes

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ let nav = document.querySelector(".nav-links");
 let menu = document.querySelector(".menu");
 let menuicon = document.querySelector(".menu-icon");
 
-menu.addEventListener("click", function () {
-  if (nav.style.transform === "translateX(100%)") {
-    nav.style.transform = "translateX(0)";
-    nav.style.display = "flex";
-    menuicon.innerHTML = "close";
-  } else {
-    nav.style.transform = "translateX(100%)";
-    nav.style.display = "none";
-    menuicon.innerHTML = "menu";
+let menuToggler = false;
+
+function toggleMenu() {
+  menuToggler = !menuToggler;
+
+  function activate() {
+    if (menuToggler === true) {
+      nav.style.transform = "translateX(0)";
+      nav.style.display = "flex";
+      menuicon.innerHTML = "close";
+    } else {
+      nav.style.transform = "translateX(100%)";
+      nav.style.display = "none";
+      menuicon.innerHTML = "menu";
+    }
   }
-});
+  return activate();
+}
